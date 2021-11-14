0d168a5e3f055babc40c48b0fcde982d8baa24a2
Mattsi <mattsi.jansky@codurance.com>
2021-11-14 16:00:59 +0000

    More comprehensive tests

--- tests/acceptance.rs
+++ tests/acceptance.rs
@@ -11,14 +11,14 @@ fn new_game_has_blank_pgn() {
     assert_eq!(result,"");
 }
 
-#[test_case("a4")]
-#[test_case("b4")]
-#[test_case("c4")]
-#[test_case("d4")]
-#[test_case("e4")]
-#[test_case("f4")]
-#[test_case("g4")]
-#[test_case("h4")]
+#[test_case("a3")]
+#[test_case("b3")]
+#[test_case("c3")]
+#[test_case("d3")]
+#[test_case("e3")]
+#[test_case("f3")]
+#[test_case("g3")]
+#[test_case("h3")]
 fn first_pawn_move_recorded_in_pgn(san: &str) {
     let mut game = ChessGame::new();
 
@@ -28,13 +28,28 @@ fn first_pawn_move_recorded_in_pgn(san: &str) {
     assert_eq!(format!("1. {} *", san), result);
 }
 
-#[test]
-fn second_pawn_move_recorded_in_pgn() {
+#[test_case("a5")]
+#[test_case("b5")]
+#[test_case("c5")]
+#[test_case("d5")]
+#[test_case("e5")]
+#[test_case("f5")]
+#[test_case("g5")]
+#[test_case("h5")]
+#[test_case("a6")]
+#[test_case("b6")]
+#[test_case("c6")]
+#[test_case("d6")]
+#[test_case("e6")]
+#[test_case("f6")]
+#[test_case("g6")]
+#[test_case("h6")]
+fn second_pawn_move_recorded_in_pgn(san: &str) {
     let mut game = ChessGame::new();
 
     game = game.make_move("e4").unwrap();
-    game = game.make_move("e5").unwrap();
+    game = game.make_move(san).unwrap();
     let result = game.get_pgn();
 
-    assert_eq!(format!("1. e4 {} *", "e5"), result);
+    assert_eq!(format!("1. e4 {} *", san), result);
 }
