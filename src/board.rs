0c3dbc7d6dc9c174aa2260324a900ba4334c8e94
Mattsi <mattsi.jansky@codurance.com>
2021-11-15 19:34:53 +0000

    Add board for recording gamestate

---
+++ src/board.rs
@@ -0,0 +1,66 @@
+use std::ops::{Index, IndexMut};
+use crate::piece::ChessPiece;
+
+pub struct Board {
+    state: [Option<ChessPiece>; 8*8]
+}
+
+impl Board {
+    fn blank() -> Board {
+        Board {
+            state: [
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+                None,None,None,None,None,None,None,None,
+            ]
+        }
+    }
+}
+
+impl Index<(usize,usize)> for Board {
+    type Output = Option<ChessPiece>;
+    fn index(&self, s: (usize,usize)) -> &Option<ChessPiece> {
+        return &self.state[s.0 * s.1];
+    }
+}
+
+impl IndexMut<(usize,usize)> for Board {
+    fn index_mut(&mut self, s: (usize,usize)) -> &mut Option<ChessPiece> {
+        return &mut self.state[s.0 * s.1];
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use galvanic_assert::assert_that;
+    use galvanic_assert::matchers::*;
+
+    use crate::piece::ChessPieceType;
+    use super::*;
+
+    #[test]
+    fn given_empty_tile_try_get_value_via_index() {
+        let board = Board::blank();
+
+        let result = board[(0,0)];
+
+        assert!(result.is_none())
+    }
+
+    #[test]
+    fn insert_piece_into_board_via_index() {
+        let mut board = Board::blank();
+
+        board[(0,0)] = Some(ChessPiece::new(true, ChessPieceType::King));
+        let result = board[(0,0)];
+
+        assert!(!result.is_none());
+        let result = result.unwrap();
+        assert_that!(&result.piece_type, eq(ChessPieceType::King));
+    }
+}
---
+++ src/piece.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum ChessPieceType {
+    Pawn,
+    Bishop,
+    Knight,
+    Rook,
+    King,
+    Queen
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct ChessPiece {
+    pub is_owned_by_first_player: bool,
+    pub piece_type: ChessPieceType,
+}
+
+impl ChessPiece {
+    pub fn new(is_owned_by_first_player: bool, piece_type: ChessPieceType) -> ChessPiece {
+        ChessPiece {is_owned_by_first_player, piece_type}
+    }
+}
