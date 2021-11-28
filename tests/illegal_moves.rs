bbc74fd7057764cace6ecf1f08d553038d114c58
Mattsi <mattsi.jansky@codurance.com>
2021-11-28 16:01:44 +0000

    Improve coverage of illegal moves

--- tests/illegal_moves.rs
+++ tests/illegal_moves.rs
@@ -3,10 +3,25 @@ use test_case::test_case;
 use chess::game::Game;
 
 #[test_case("e7")]
+#[test_case("ng3")]
+#[test_case("bc2")]
+#[test_case("qe3")]
+#[test_case("banana")]
 fn cannot_make_illegal_move(illegal_move: &str) {
     let game = Game::new();
 
     let result = game.make_move(illegal_move);
 
     assert!(result.is_err());
+}
+
+#[test]
+fn cannot_move_off_board() {
+    let game = Game::from_fen("8/8/8/8/8/8/8/3Q4 w - - 0 1");
+
+    let result = game.make_move("qd9");
+    let result2 = game.make_move("qd-1");
+
+    assert!(result.is_err());
+    assert!(result2.is_err());
 }
