bee3a14f8b838da402bc30c9a517e36e4be07ba0
Mattsi <mattsi.jansky@codurance.com>
2021-11-30 19:46:55 +0000

    Bit more development on the CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,14 +1,25 @@
 mod fen_display;
 mod terminal;
 
+use std::io::Write;
 use console::Term;
 use iroh::game::Game;
 use crate::fen_display::generate_display_from_fen;
 
 fn main() {
-    let term = Term::stdout();
+    let mut term = Term::stdout();
+    term.clear_screen();
 
-    let game = Game::new();
+    let mut game = Game::new();
+    render(&term, &game);
+    term.write_line("");
+    term.write("Your move: ".as_bytes());
+
+    let read = term.read_line();
+    term.clear_last_lines(8);
+}
+
+fn render(term: &Term, game: &Game) {
     let fen = game.generate_fen();
     let display = generate_display_from_fen(&fen[..]);
 
