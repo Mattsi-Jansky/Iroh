8b8cbe5f51597be6f3bcb7a6da0bad48826c7fd2
Mattsi <mattsi.jansky@codurance.com>
2021-12-04 14:15:43 +0000

    Implement first player cannot castling kingside

--- iroh/src/moves/resolve_move.rs
+++ iroh/src/moves/resolve_move.rs
@@ -2,15 +2,15 @@ use crate::state::board::Board;
 use crate::state::coordinates::{File, Rank};
 use crate::moves::Move;
 use crate::state::GameState;
-use crate::state::piece::Piece;
+use crate::state::piece::{Piece, PieceType};
 
 pub fn resolve_move(requested_move: Move, mut game_state: GameState) -> GameState {
     match requested_move {
         Move::PawnMove(from, to_rank) => {
-            move_piece(&mut game_state.board, from.0, from.1, from.0, to_rank);
+            move_piece(&mut game_state, from.0, from.1, from.0, to_rank);
         }
         Move::RegularMove((from_file, from_rank), (to_file, to_rank), _) => {
-            move_piece(&mut game_state.board, from_file, from_rank, to_file, to_rank);
+            move_piece(&mut game_state, from_file, from_rank, to_file, to_rank);
         }
         Move::AttackMove((from_file, from_rank), (to_file, to_rank), _) => {
             let piece = game_state.board[(to_file,to_rank)]
@@ -19,7 +19,7 @@ pub fn resolve_move(requested_move: Move, mut game_state: GameState) -> GameStat
             if game_state.is_first_player_turn() {game_state.captured_pieces.second_player.push(piece);}
             else {game_state.captured_pieces.first_player.push(piece);}
             
-            move_piece(&mut game_state.board, from_file, from_rank, to_file, to_rank);
+            move_piece(&mut game_state, from_file, from_rank, to_file, to_rank);
         }
         Move::PawnAttackMove(starting_file,(to_file, to_rank)) => {
             let piece = game_state.board[(to_file,to_rank)]
@@ -29,7 +29,7 @@ pub fn resolve_move(requested_move: Move, mut game_state: GameState) -> GameStat
             else {game_state.captured_pieces.first_player.push(piece);}
 
             let direction = if game_state.is_first_player_turn() {-1} else {1};
-            move_piece(&mut game_state.board,
+            move_piece(&mut game_state,
                        starting_file,
                        to_rank.transform(direction)
                            .expect("should not be possible to get an out of bounds pawn attack command"),
@@ -59,10 +59,25 @@ pub fn resolve_move(requested_move: Move, mut game_state: GameState) -> GameStat
     game_state
 }
 
-fn move_piece(board: &mut Board, from_file: File, from_rank: Rank, to_file: File, to_rank: Rank) {
+fn move_piece(game_state: &mut GameState, from_file: File, from_rank: Rank, to_file: File, to_rank: Rank) {
     let from = (from_file, from_rank);
     let to = (to_file, to_rank);
-    let piece = board[from];
-    board[from] = None;
-    board[to] = piece;
+    let piece = game_state.board[from];
+    game_state.board[from] = None;
+    game_state.board[to] = piece;
+
+    update_castling_state(game_state, from, piece)
+}
+
+fn update_castling_state(game_state: &mut GameState, from: (File, Rank), piece: Option<Piece>) {
+    let moving_piece_type = piece.unwrap().piece_type;
+    if moving_piece_type == PieceType::Rook
+        && from.0 == File::new(7)
+        && from.1 == Rank::new(0) {
+        game_state.first_player_can_castle_kingside = false;
+    } else if moving_piece_type == PieceType::King
+        && from.0 == File::new(4)
+        && from.1 == Rank::new(0) {
+        game_state.first_player_can_castle_kingside = false;
+    }
 }
--- iroh/tests/castling.rs
+++ iroh/tests/castling.rs
@@ -8,3 +8,50 @@ fn given_first_player_should_castle_kingside() {
     assert_eq!("1. O-O *", game.get_pgn());
     assert_eq!("rnbqk1nr/ppp1bppp/3p4/4p3/4P3/3B1N2/PPPP1PPP/RNBQ1RK1 b kq - 0 1", game.generate_fen());
 }
+
+#[test]
+fn given_first_player_without_clear_path_cannot_castle_kingside() {
+    let game = Game::from_fen("rnbqk1nr/ppp1bppp/3p4/4p3/4P3/2NB4/PPPP1PPP/R1BQK1NR w KQkq - 0 1");
+
+    let result = game.make_move("O-O");
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn given_first_player_and_castle_not_in_place_cannot_castle_kingside() {
+    let mut game = Game::from_fen("rnbqk1nr/ppp1bppp/3p4/4p3/4P3/3B1N2/PPPP1PPP/RNBQK2R w KQkq - 0 1");
+
+    game = game.make_move("Rg1").unwrap();
+    let result = game.make_move("O-O");
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn given_first_player_and_castle_has_moved_cannot_castle_kingside() {
+    let mut game = Game::from_fen("rnbqk1nr/ppp1bppp/3p4/4p3/4P3/3B1N2/PPPP1PPP/RNBQK2R w KQkq - 0 1");
+
+    game = game.make_move("Rg1").unwrap();
+    game = game.make_move("a6").unwrap();
+    game = game.make_move("Rh1").unwrap();
+    game = game.make_move("b6").unwrap();
+    let result = game.make_move("O-O");
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn given_first_player_and_king_has_moved_cannot_castle_kingside() {
+    let mut game = Game::from_fen("rnbqk1nr/ppp1bppp/3p4/4p3/4P3/3B1N2/PPPP1PPP/RNBQK2R w KQkq - 0 1");
+
+    game = game.make_move("Kf1").unwrap();
+    game = game.make_move("a6").unwrap();
+    game = game.make_move("Ke1").unwrap();
+    game = game.make_move("b6").unwrap();
+    let result = game.make_move("O-O");
+
+    assert!(result.is_err());
+}
+
+//Given FEN says you can't castle kingside
