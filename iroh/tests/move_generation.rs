010cf583a027e69df94c09c0d0353be7e693d6ce
Mattsi <mattsi.jansky@codurance.com>
2022-05-21 16:52:38 +0100

    Remove commented out old test

--- iroh/tests/move_generation.rs
+++ iroh/tests/move_generation.rs
@@ -184,21 +184,3 @@ fn pawn_attack_moves() {
         Move::PawnAttackMove { 0: File::new(3), 1: (File::new(4),Rank::new(5)) },
     ]));
 }
-
-// #[test]
-// fn do_not_generate_moves_that_put_you_in_check() {
-//     let mut game = Game::from_fen("8/4k3/3r4/8/8/5R2/4K3/8 w - - 0 1");
-//
-//     let available_moves_san : Vec<String> = game.get_available_moves().iter().map(|m| m.generate_san()).collect();
-//
-//     println!("{:?}",available_moves_san);
-//     assert_that!(&available_moves_san,
-//         all_elements_satisfy(|m| m != "Kd3")
-//     );
-//     assert_that!(&available_moves_san,
-//         all_elements_satisfy(|m| m != "Kd2")
-//     );
-//     assert_that!(&available_moves_san,
-//         all_elements_satisfy(|m| m != "Kd1")
-//     );
-// }
