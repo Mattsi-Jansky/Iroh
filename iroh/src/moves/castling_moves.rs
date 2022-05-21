3f56e359779f4563c17e6d5aaab35a1295f1c522
Mattsi <mattsi.jansky@codurance.com>
2022-05-21 17:10:32 +0100

    Minor refactors to castling_moves.rs

--- iroh/src/moves/castling_moves.rs
+++ iroh/src/moves/castling_moves.rs
@@ -6,12 +6,12 @@ use crate::state::piece::PieceType;
 pub fn generate_castling_moves(available_moves: &mut Vec<Move>, game_state: &GameState) {
 
     if game_state.is_first_player_turn {
-        let e1 = game_state.board[(File::new(4),Rank::new(0))];
+        let e1 = game_state.board[(File::E,Rank::ONE)];
 
         if game_state.first_player_can_castle_kingside {
-            let f1 = game_state.board[(File::new(5), Rank::new(0))];
-            let g1 = game_state.board[(File::new(6), Rank::new(0))];
-            let h1 = game_state.board[(File::new(7), Rank::new(0))];
+            let g1 = game_state.board[(File::G, Rank::ONE)];
+            let f1 = game_state.board[(File::F, Rank::ONE)];
+            let h1 = game_state.board[(File::H, Rank::ONE)];
 
             if let (Some(e1_piece), Some(h1_piece)) = (e1, h1) {
                 if e1_piece.piece_type == PieceType::King
@@ -24,10 +24,10 @@ pub fn generate_castling_moves(available_moves: &mut Vec<Move>, game_state: &Gam
         }
 
         if game_state.first_player_can_castle_queenside {
-            let a1 = game_state.board[(File::new(0), Rank::new(0))];
-            let b1 = game_state.board[(File::new(1), Rank::new(0))];
-            let c1 = game_state.board[(File::new(2), Rank::new(0))];
-            let d1 = game_state.board[(File::new(3), Rank::new(0))];
+            let a1 = game_state.board[(File::A, Rank::ONE)];
+            let b1 = game_state.board[(File::B, Rank::ONE)];
+            let c1 = game_state.board[(File::C, Rank::ONE)];
+            let d1 = game_state.board[(File::D, Rank::ONE)];
 
             if let (Some(a1_piece), Some(e1_piece)) = (a1, e1) {
                 if e1_piece.piece_type == PieceType::King
@@ -40,12 +40,12 @@ pub fn generate_castling_moves(available_moves: &mut Vec<Move>, game_state: &Gam
             }
         }
     } else {
-        let e8 = game_state.board[(File::new(4),Rank::new(7))];
+        let e8 = game_state.board[(File::E,Rank::EIGHT)];
 
         if game_state.second_player_can_castle_kingside {
-            let f8 = game_state.board[(File::new(5), Rank::new(7))];
-            let g8 = game_state.board[(File::new(6), Rank::new(7))];
-            let h8 = game_state.board[(File::new(7), Rank::new(7))];
+            let f8 = game_state.board[(File::F, Rank::EIGHT)];
+            let g8 = game_state.board[(File::G, Rank::EIGHT)];
+            let h8 = game_state.board[(File::H, Rank::EIGHT)];
 
             if let (Some(e8_piece), Some(h8_piece)) = (e8, h8) {
                 if e8_piece.piece_type == PieceType::King
@@ -58,10 +58,10 @@ pub fn generate_castling_moves(available_moves: &mut Vec<Move>, game_state: &Gam
         }
 
         if game_state.second_player_can_castle_queenside {
-            let a8 = game_state.board[(File::new(0), Rank::new(7))];
-            let b8 = game_state.board[(File::new(1), Rank::new(7))];
-            let c8 = game_state.board[(File::new(2), Rank::new(7))];
-            let d8 = game_state.board[(File::new(3), Rank::new(7))];
+            let a8 = game_state.board[(File::A, Rank::EIGHT)];
+            let b8 = game_state.board[(File::B, Rank::EIGHT)];
+            let c8 = game_state.board[(File::C, Rank::EIGHT)];
+            let d8 = game_state.board[(File::D, Rank::EIGHT)];
 
             if let (Some(a8_piece), Some(e8_piece)) = (a8, e8) {
                 if e8_piece.piece_type == PieceType::King
