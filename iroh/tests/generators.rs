d2066625fbb9cae7a89018afb89eebd39c5a8a35
Mattsi <mattsi.jansky@codurance.com>
2022-05-28 17:52:17 +0100

    Refactor chess_test!, DRY

--- iroh/tests/generators.rs
+++ iroh/tests/generators.rs
@@ -1,38 +1,22 @@
 #[macro_export]
 macro_rules! chess_test_inner {
-    ($name:ident,[$($san:literal),+],$pgn:expr) => {
-        #[test]
-        fn $name() {
-            let mut game = Game::new();
-
-            $(game = game.make_move($san).unwrap();)+
-            let result = game.get_pgn();
-
-            assert_eq!($pgn, result);
-        }
+    ($name:ident,$sans:tt,$pgn:expr) => {
+        chess_test_inner! { $name @ "",$sans,$pgn,"" }
     };
-    ($name:ident @ $fen:expr,[$($san:literal),+],$pgn:expr) => {
-        #[test]
-        fn $name() {
-            let mut game = Game::from_fen($fen);
-
-            $(game = game.make_move($san).unwrap();)+
-            let result = game.get_pgn();
-
-            assert_eq!($pgn, result);
-        }
+    ($name:ident @ $fen:expr,$sans:tt,$pgn:expr) => {
+        chess_test_inner! {$name @ $fen,$sans,$pgn,""}
     };
     ($name:ident @ $fen:expr,[$($san:literal),+],$pgn:expr,$expected_fen:expr) => {
         #[test]
         fn $name() {
-            let mut game = Game::from_fen($fen);
+            let mut game = if $fen.is_empty() {Game::new()} else {Game::from_fen($fen)};
 
             $(game = game.make_move($san).unwrap();)+
             let result_pgn = game.get_pgn();
             let result_fen = game.generate_fen();
 
             assert_eq!($pgn, result_pgn);
-            assert_eq!($expected_fen,result_fen);
+            if!$expected_fen.is_empty() {assert_eq!($expected_fen,result_fen)}
         }
     }
 }
