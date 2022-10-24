bd333dd14cf5029fce7177f3dede7f9da19ffebb
Mattsi <mattsi.jansky@codurance.com>
2022-10-24 19:01:31 +0100

    Refactor evaluation tests to use macro

--- iroh/tests/evaluation.rs
+++ iroh/tests/evaluation.rs
@@ -1,11 +1,25 @@
 use iroh::evaluations::evaluate;
 use iroh::state::GameState;
 
-#[test]
-fn simple_puzzle() {
-    let state = GameState::from_fen("3k4/8/8/3p4/4P3/8/8/3K4 w - - 0 1");
+macro_rules! evaluation_test_inner {
+    ($name:ident,$fen:expr,$expected_san:expr) => {
+        #[test]
+        fn $name() {
+            let state = GameState::from_fen($fen);
 
-    let result = evaluate(&state);
+            let result = evaluate(&state);
 
-    assert_eq!(String::from("exd5"), result.best_move);
+            assert_eq!(String::from($expected_san), result.best_move);
+        }
+    }
+}
+
+macro_rules! evaluation_tests {
+    ($($myblock:tt)*) => {
+        $(evaluation_test_inner! $myblock)*
+    };
+}
+
+evaluation_tests! {
+    {simple_puzzle,"3k4/8/8/3p4/4P3/8/8/3K4 w - - 0 1","exd5"}
 }
