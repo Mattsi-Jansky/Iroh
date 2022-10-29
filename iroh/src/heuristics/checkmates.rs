d2addec7dc07ccae3102c6a66ab444f862d3f9bc
Mattsi <mattsi.jansky@codurance.com>
2022-10-29 17:50:30 +0100

    Implement negative heuristic for self checkmate

--- iroh/src/heuristics/checkmates.rs
+++ iroh/src/heuristics/checkmates.rs
@@ -8,12 +8,12 @@ pub struct OpponentInCheckmateHeuristic {}
 
 impl Heuristic for OpponentInCheckmateHeuristic {
     fn evaluate(&self, state: &GameState, is_first_player: bool, cache: &HeuristicsCache) -> i32 {
-        let not_our_turn = state.is_first_player_turn != is_first_player;
+        let not_my_turn = state.is_first_player_turn != is_first_player;
         let has_no_moves = cache.has_no_moves;
         let opponent_checked = is_first_player && cache.is_check_second_player
             || !is_first_player && cache.is_check_first_player;
-        
-        if not_our_turn && has_no_moves && opponent_checked {
+
+        if not_my_turn && has_no_moves && opponent_checked {
             VERY_BIG_NUMBER
         } else {
             0
@@ -25,6 +25,27 @@ impl Heuristic for OpponentInCheckmateHeuristic {
     }
 }
 
+pub struct SelfInCheckmateHeuristic {}
+
+impl Heuristic for SelfInCheckmateHeuristic {
+    fn evaluate(&self, state: &GameState, is_first_player: bool, cache: &HeuristicsCache) -> i32 {
+        let is_my_turn = state.is_first_player_turn == is_first_player;
+        let has_no_moves = cache.has_no_moves;
+        let self_checked = is_first_player && cache.is_check_first_player
+            || !is_first_player && cache.is_check_second_player;
+
+        if is_my_turn && has_no_moves && self_checked {
+            -VERY_BIG_NUMBER
+        } else {
+            0
+        }
+    }
+
+    fn get_type(&self) -> HeuristicType {
+        HeuristicType::SelfInCheckMate
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,9 +54,12 @@ mod tests {
     fn given_not_in_checkmate_value_0() {
         let state = GameState::new();
 
-        let result = OpponentInCheckmateHeuristic{}.evaluate(&state, true, &HeuristicsCache::from(&state));
+        let result_opponent = OpponentInCheckmateHeuristic{}.evaluate(&state, true, &HeuristicsCache::from(&state));
+        let result_self = SelfInCheckmateHeuristic{}.evaluate(&state, true, &HeuristicsCache::from(&state));
 
-        assert_eq!(0, result);
+        assert_eq!(0, result_opponent);
+        assert_eq!(0, result_self);
+
     }
 
     #[test]
@@ -45,5 +69,14 @@ mod tests {
         let result = OpponentInCheckmateHeuristic{}.evaluate(&state, true, &HeuristicsCache::from(&state));
 
         assert_eq!(VERY_BIG_NUMBER, result);
+    }
+
+    #[test]
+    fn given_self_in_checkmate_return_negative_extremely_big_value() {
+        let state = GameState::from_fen("R2k4/7R/8/8/8/8/8/3K4 b - - 0 1");
+
+        let result = SelfInCheckmateHeuristic{}.evaluate(&state, false, &HeuristicsCache::from(&state));
+
+        assert_eq!(-VERY_BIG_NUMBER, result);
     }
 }
