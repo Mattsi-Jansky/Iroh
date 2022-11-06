0a6a56c85c1ee8395a3dac5b8e585aef2faea356
Mattsi <mattsi.jansky@codurance.com>
2022-11-06 15:12:51 +0000

    Minor search refactors for readability, remove logs

--- iroh/src/search/mod.rs
+++ iroh/src/search/mod.rs
@@ -35,41 +35,28 @@ pub fn search(game: &Game) -> Evaluation {
 fn minmax(game:&Game, depth: u8, is_maximising: bool, heuristics: &Heuristics, mut alpha: i32, mut beta: i32) -> i32 {
     let is_ongoing = !matches!(game, Game::Ongoing {..});
     let state = &game.unwrap();
-    #[cfg(debug_assertions)]
-    println!("minmaxing {:?} with alpha {}, beta {}, maximising {:?}, FEN {}", state.sans, alpha, beta, is_maximising, state.generate_fen());
-    if depth == MAX_DEPTH || is_ongoing {
-        let value = heuristics.evaluate(state);
-        #[cfg(debug_assertions)]
-        println!("=== VALUE: {}, SAN: {:?}, FEN: {}", value, state.sans, state.generate_fen());
-        value
-    } else {
+    if depth == MAX_DEPTH || is_ongoing { heuristics.evaluate(state) }
+    else {
         let mut best_value = if is_maximising { i32::MIN } else { i32::MAX };
         for possible_move in state.possible_moves.iter() {
-            #[cfg(debug_assertions)]
-            println!("EVALUATING {possible_move}");
             let move_result = state.make_move(&possible_move);
             let value = minmax(&move_result, depth + 1, !is_maximising, heuristics, alpha, beta);
             if (is_maximising && value > best_value)
                 || (!is_maximising && value < best_value) {
                 best_value = value;
             }
+
             if is_maximising {
                 if value >= beta {
-                    #[cfg(debug_assertions)]
-                    println!("Breaking out from {possible_move} because {value} is greater or equal to beta {beta}");
                     break;
                 }
-                alpha = if value > alpha { value } else { alpha };
-            }else {
+                alpha = i32::max(value, alpha);
+            } else {
                 if value <= alpha {
-                    #[cfg(debug_assertions)]
-                    println!("Breaking out from {possible_move} because {value} is less than or equal to beta {alpha}");
                     break;
                 }
-                beta = if value < beta { value } else { beta };
+                beta = i32::min(value, beta);
             }
-            #[cfg(debug_assertions)]
-            println!("EVALUATED {} at {}", possible_move.generate_san(), value);
         }
         best_value
     }
