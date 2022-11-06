aa9d4012d9c6240ac218e45d3d903d060910d897
Mattsi <mattsi.jansky@codurance.com>
2022-11-06 15:01:43 +0000

    Extend checkmate search improvements to 2nd player

--- iroh/src/heuristics/checkmates.rs
+++ iroh/src/heuristics/checkmates.rs
@@ -9,8 +9,7 @@ pub struct InCheckmateHeuristic {}
 impl Heuristic for InCheckmateHeuristic {
     fn evaluate(&self, state: &GameState, cache: &HeuristicsCache) -> i32 {
         if cache.is_check_first_player && cache.has_no_moves {
-
-            -VERY_BIG_NUMBER
+            -VERY_BIG_NUMBER + (state.turn_number * 10) as i32
         }
         else if cache.is_check_second_player && cache.has_no_moves {
             VERY_BIG_NUMBER - (state.turn_number * 10) as i32
@@ -51,11 +50,11 @@ mod tests {
 
         let result = InCheckmateHeuristic {}.evaluate(&state, &HeuristicsCache::from(&state));
 
-        assert_eq!(-VERY_BIG_NUMBER, result);
+        assert_eq!(-VERY_BIG_NUMBER + 10, result);
     }
 
     #[test]
-    fn reduces_heuristic_by_number_of_turns_taken() {
+    fn given_second_player_in_checkmate_should_reduce_heuristic_by_number_of_turns_taken() {
         let mut state_one_turn = GameState::from_fen("R2k4/7R/8/8/8/8/8/3K4 b - - 0 1");
         state_one_turn.turn_number = 1;
         let mut state_ten_turns = GameState::from_fen("R2k4/7R/8/8/8/8/8/3K4 b - - 0 1");
@@ -66,5 +65,19 @@ mod tests {
 
         assert_eq!(VERY_BIG_NUMBER - 10, result_one_turn);
         assert_eq!(VERY_BIG_NUMBER - 100, result_ten_turns);
+    }
+
+    #[test]
+    fn given_first_player_in_checkmate_should_reduce_heuristic_by_number_of_turns_taken() {
+        let mut state_one_turn = GameState::from_fen("r2K4/7r/8/8/8/8/8/3k4 w - - 0 1");
+        state_one_turn.turn_number = 1;
+        let mut state_ten_turns = GameState::from_fen("r2K4/7r/8/8/8/8/8/3k4 w - - 0 1");
+        state_ten_turns.turn_number = 10;
+
+        let result_one_turn = InCheckmateHeuristic {}.evaluate(&state_one_turn, &HeuristicsCache::from(&state_one_turn));
+        let result_ten_turns = InCheckmateHeuristic {}.evaluate(&state_ten_turns, &HeuristicsCache::from(&state_ten_turns));
+
+        assert_eq!(-VERY_BIG_NUMBER + 10, result_one_turn);
+        assert_eq!(-VERY_BIG_NUMBER + 100, result_ten_turns);
     }
 }
